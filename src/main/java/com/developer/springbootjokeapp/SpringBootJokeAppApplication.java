931a9c3525175a0edb81f4112c40930c2e3901b1
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-14 20:52:06 +0530

    Added XML Configuration for Chuck Norris Bean. Commented out the Java Bean declaration.

--- src/main/java/com/developer/springbootjokeapp/SpringBootJokeAppApplication.java
+++ src/main/java/com/developer/springbootjokeapp/SpringBootJokeAppApplication.java
@@ -4,15 +4,17 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = "classpath:chuck-config.xml")
 public class SpringBootJokeAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJokeAppApplication.class, args);
     }
 
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotesBean() {
         return new ChuckNorrisQuotes();
     }
