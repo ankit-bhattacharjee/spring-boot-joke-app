6d7d7ff88d8d0f295ffecd78e947b2c65d9570b9
Ankit Bhattacharjee <ankit.bhattacharjee94@gmail.com>
2019-12-14 15:09:59 +0530

    Initial Commit.

---
+++ src/test/java/com/developer/springbootjokeapp/SpringBootJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.springbootjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
